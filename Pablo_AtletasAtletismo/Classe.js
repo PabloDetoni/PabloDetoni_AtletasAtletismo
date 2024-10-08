e3c0ecf22e07c6cfaa9bb70da880e1df4bb389ab
PabloDetoni <pablodetoni10@gmail.com>
2024-10-08 15:32:15 -0300

    Add files via upload

---
+++ Pablo_AtletasAtletismo/Classe.js
@@ -0,0 +1,11 @@
+class Atleta {
+    constructor(nome, data, modalidade, genero, competicoes, medalhas) {
+
+        this.nome = nome;
+        this.data = data;
+        this.modalidade = modalidade;
+        this.genero = genero;
+        this.competicoes = competicoes;
+        this.medalhas = medalhas;
+    }
+}
