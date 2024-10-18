7e0e61cf828d7cf18a4dba5c9cfd7833dbdb2788
PabloDetoni <pablodetoni10@gmail.com>
2024-10-18 09:51:59 -0300

    Classe.js

--- Pablo_AtletasAtletismo/Classe.js
+++ Pablo_AtletasAtletismo/Classe.js
@@ -1,11 +1,13 @@
 class Atleta {
-    constructor(nome, data, modalidade, genero, competicoes, medalhas) {
-
+    constructor(id, nome, data, modalidade, genero, competicoes, medalhas, posicaoNaLista) {
+        this.id = id;
         this.nome = nome;
         this.data = data;
         this.modalidade = modalidade;
         this.genero = genero;
         this.competicoes = competicoes;
         this.medalhas = medalhas;
+
+        this.posicaoNaLista = posicaoNaLista
     }
 }
