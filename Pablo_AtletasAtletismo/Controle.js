90a1b5e6c6b61ec120c1b4d7f596d4277d57b786
PabloDetoni <pablodetoni10@gmail.com>
2024-10-22 15:35:29 -0300

    Controle.js

--- Pablo_AtletasAtletismo/Controle.js
+++ Pablo_AtletasAtletismo/Controle.js
@@ -261,3 +261,280 @@ function visibilidadeDosBotoes(btProcure, btInserir, btAlterar, btExcluir, btSal
     document.getElementById("btCancelar").style.display = btSalvar; // o cancelar sempre aparece junto com o salvar
     document.getElementById("inputId").focus();
 }
+/*
+let listaDeAtletas = []; // conjunto de dados
+let oQueEstaFazendo = ''; // variável global de controle
+let atleta = null; // variável global 
+bloquearAtributos(true);
+
+// backend (não interage com o html)
+function procurePorChavePrimaria(chave) {
+    for (let i = 0; i < listaDeAtletas.length; i++) {
+        const atleta = listaDeAtletas[i];
+        if (atleta.id == chave) {
+            atleta.posicaoNaLista = i;
+            return listaDeAtletas[i];
+        }
+    }
+    return null; // não achou
+}
+
+function descobrirIdade() {
+    let data = document.getElementById("inputData").value; // Alterado para let
+    data = new Date(data).getFullYear();
+    let idade = 2024 - data;
+    return idade;
+}
+
+function categorizar(idade) {
+    let categ;
+
+    if (idade === 12 || idade === 13) {
+        categ = "Categoria Sub-14";
+    } else if (idade === 14 || idade === 15) {
+        categ = "Categoria Sub-16";
+    } else if (idade === 16 || idade === 17) {
+        categ = "Categoria Sub-18";
+    } else if (idade === 18 && idade === 19) {
+        categ = "Categoria Sub-20";
+    } else if (idade >= 20 && idade <= 22) { // Corrigido para idade >= 20
+        categ = "Categoria Sub-23";
+    } else if (idade >= 40) {
+        categ = "Categoria de Masters";
+    } else {
+        categ = "Categoria de Adultos"; // Mover aqui
+    }
+
+    return categ;
+}
+
+function descobrirGenero() {
+    let genero; // Declarada a variável
+    let g = document.getElementById("inputGenero").value;
+    if (g === "fem") {
+        genero = "Feminino";
+    } else {
+        genero = "Masculino";
+    }
+    return genero;
+}
+
+function descobrirModalidade() {
+    let modalidade; // Declarada a variável
+    let M = document.getElementById("inputModalidade").value;
+    if (M === "inputCV") {
+        modalidade = "Corrida de Velocidade";
+    } else if (M === "inputCR") {
+        modalidade = "Corrida de Resistência";
+    } else if (M === "inputLA") {
+        modalidade = "Lançamento ou Arremesso";
+    } else {
+        modalidade = "Salto";
+    }
+    return modalidade; // Adicionada para retornar o valor
+}
+
+// Função para procurar um elemento pela chave primária
+function procure() {
+    const id = document.getElementById("inputId").value;
+    if (id) { // se digitou um id
+        atleta = procurePorChavePrimaria(id);
+        if (atleta) { // achou na lista
+            mostrarDadosAtleta(atleta);
+            visibilidadeDosBotoes('inline', 'none', 'inline', 'inline', 'none'); // Habilita botões de alterar e excluir
+            mostrarAviso("Achou na lista, pode alterar ou excluir");
+        } else { // não achou na lista
+            limparAtributos();
+            visibilidadeDosBotoes('inline', 'inline', 'none', 'none', 'none');
+            mostrarAviso("Não achou na lista, pode inserir");
+        }
+    } else {
+        document.getElementById("inputId").focus();
+        return;
+    }
+}
+
+// backend->frontend
+function inserir() {
+    bloquearAtributos(false);
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline'); // visibilidade dos botões
+    oQueEstaFazendo = 'inserindo';
+    mostrarAviso("INSERINDO - Digite os atributos e clique o botão salvar");
+    document.getElementById("inputId").focus();
+}
+
+// Função para alterar um elemento da lista
+function alterar() {
+    // Remove o readonly dos campos
+    bloquearAtributos(false);
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline');
+    oQueEstaFazendo = 'alterando';
+    mostrarAviso("ALTERANDO - Digite os atributos e clique o botão salvar");
+}
+
+// Função para excluir um elemento da lista
+function excluir() {
+    bloquearAtributos(false);
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline');
+    oQueEstaFazendo = 'excluindo';
+    mostrarAviso("EXCLUINDO - clique o botão salvar para confirmar a exclusão");
+}
+
+function salvar() {
+    // Obter os dados a partir do html
+    let id;
+    if (atleta == null) {
+        id = document.getElementById("inputId").value;
+    } else {
+        id = atleta.id;
+    }
+
+    const nome = document.getElementById("inputNome").value;
+    const idade = descobrirIdade(); // Chamando a função para obter a idade
+    const modalidade = descobrirModalidade(); // Chamando a função para obter a modalidade
+    const genero = descobrirGenero(); // Chamando a função para obter o gênero
+    const competicoes = parseInt(document.getElementById("inputCompeticoes").value);
+    const medalhas = parseInt(document.getElementById("inputMedalhas").value);
+
+    // Verificar se o que foi digitado pelo USUÁRIO está correto
+    if (id && nome && idade && medalhas && modalidade && genero && competicoes) { // se tudo certo 
+        switch (oQueEstaFazendo) {
+            case 'inserindo':
+                atleta = new Atleta(id, nome, idade, medalhas, modalidade, genero, competicoes); // Usando Atleta com letra maiúscula
+                listaDeAtletas.push(atleta);
+                mostrarAviso("Inserido na lista");
+                break;
+            case 'alterando':
+                atletaAlterado = new Atleta(id, nome, idade, medalhas, modalidade, genero, competicoes);
+                listaDeAtletas[atleta.posicaoNaLista] = atletaAlterado;
+                mostrarAviso("Alterado");
+                break;
+            case 'excluindo':
+                let novaLista = [];
+                for (let i = 0; i < listaDeAtletas.length; i++) {
+                    if (atleta.posicaoNaLista != i) {
+                        novaLista.push(listaDeAtletas[i]);
+                    }
+                }
+                listaDeAtletas = novaLista;
+                mostrarAviso("EXCLUÍDO");
+                break;
+            default:
+                mostrarAviso("Erro aleatório");
+        }
+        visibilidadeDosBotoes('inline', 'none', 'none', 'none', 'none');
+        limparAtributos();
+        listar();
+        document.getElementById("inputId").focus();
+    } else {
+        alert("Erro nos dados digitados");
+        return;
+    }
+}
+
+// backend
+function preparaListagem(vetor) {
+    let texto = "";
+    for (let i = 0; i < vetor.length; i++) {
+        const linha = vetor[i];
+        texto +=
+            linha.id + " - " +
+            linha.nome + " - " +
+            linha.idade + " - " +
+            linha.categ + " - " +
+            linha.medalhas + " - " +
+            linha.modalidade + " - " +
+            linha.genero + " - " +
+            linha.competicoes + "<br>";
+    }
+    return texto;
+}
+
+// backend->frontend (interage com html)
+function listar() {
+    document.getElementById("divResp").innerHTML = preparaListagem(listaDeAtletas);
+}
+
+function cancelarOperacao() {
+    limparAtributos();
+    bloquearAtributos(true);
+    visibilidadeDosBotoes('inline', 'none', 'none', 'none', 'none');
+    mostrarAviso("Cancelou a operação de edição");
+}
+
+function mostrarAviso(mensagem) {
+    // printa a mensagem na divAviso
+    document.getElementById("divAviso").innerHTML = mensagem;
+}
+
+// Função para mostrar os dados do atleta nos campos
+function mostrarDadosAtleta(atleta) {
+    document.getElementById("inputId").value = atleta.id;
+    document.getElementById("inputNome").value = atleta.nome;
+    document.getElementById("inputData").value = atleta.data;
+    document.getElementById("inputModalidade").value = atleta.modalidade;
+    document.getElementById("inputGenero").value = atleta.genero;
+    document.getElementById("inputMedalhas").value = atleta.medalhas;
+    document.getElementById("inputCompeticoes").value = atleta.competicoes;
+
+    // Define os campos como readonly
+    bloquearAtributos(true);
+}
+
+// Função para limpar os dados dos campos
+function limparAtributos() {
+    document.getElementById("inputNome").value = "";
+    document.getElementById("inputData").value = "";
+    document.getElementById("inputModalidade").value = "";
+    document.getElementById("inputGenero").value = "";
+    document.getElementById("inputMedalhas").value = "";
+    document.getElementById("inputCompeticoes").value = "";
+
+    bloquearAtributos(true);
+}
+
+function bloquearAtributos(soLeitura) {
+    // quando a chave primaria possibilita edicao, tranca (readonly) os outros e vice-versa
+    document.getElementById("inputId").readOnly = !soLeitura;
+    document.getElementById("inputNome").readOnly = soLeitura;
+    document.getElementById("inputData").readOnly = soLeitura;
+    document.getElementById("inputModalidade").readOnly = soLeitura;
+    document.getElementById("inputGenero").readOnly = soLeitura;
+    document.getElementById("inputMedalhas").readOnly = soLeitura;
+    document.getElementById("inputCompeticoes").readOnly = soLeitura;
+}
+
+// Função para deixar visível ou invisível os botões
+function visibilidadeDosBotoes(btProcure, btInserir, btAlterar, btExcluir, btSalvar) {
+    document.getElementById("btProcure").style.display = btProcure;
+    document.getElementById("btInserir").style.display = btInserir;
+    document.getElementById("btAlterar").style.display = btAlterar;
+    document.getElementById("btExcluir").style.display = btExcluir;
+    document.getElementById("btSalvar").style.display = btSalvar;
+    document.getElementById("btCancelar").style.display = btSalvar; // o cancelar sempre aparece junto com o salvar
+    document.getElementById("inputId").focus();
+}
+
+function inserirDadosIniciais() {
+    //esta função é para não ter que ficar digitando dados a cada vez que 
+    //recarrega a página. Facilita os testes. 
+
+    listaDeAtletas = [];//se houver dados na lista, apaga todos
+    let atleta = new Atleta('111', 'Ana Silva', 14, "Categoria Sub-16", "11/01/1111",);
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('222', 'Bruno Costa', 12, "Categoria Sub-14", "11/01/1111");
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('333', 'Carla Oliveira', 16, "Categoria Sub-18","11/01/1111");
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('444', 'Daniel Souza', 14,"Categoria Sub-16","11/01/1111");
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('555', 'Eduardo Lima', 25, "Categoria Masters" ,"11/01/1111");
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('666', 'Bruno Costa', 19, "Categoria Sub-20","11/01/1111");
+    listaDeAtletas.push(atleta);
+    atleta = new Atleta('777', 'João Lucas', 15, "Categoria Sub-16","11/01/1111");
+    listaDeAtletas.push(atleta);
+    listar();
+    visibilidadeDosBotoes('inline', 'none', 'none', 'none', 'none');
+    bloquearAtributos(true);
+}
